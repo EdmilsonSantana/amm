1f9c2c1910dae01186218524fdf5e02c939328f1
Edmilson <edmilsonsantana2@hotmail.com>
2017-07-08 02:48:37 -0300

    Utilizando cubes com Docker.

--- app/cmd.sh
+++ app/cmd.sh
@@ -4,4 +4,11 @@ set -e
 
 cp /cubes/slicer.ini.sample /cubes/slicer.ini && \
 sed -i "s|{{DB_URL}}|$DB_URL|;" /cubes/slicer.ini && \
-slicer serve /cubes/slicer.ini
+slicer serve ./slicer.ini
+
+#export CUBESVIEWER_CUBES_URL="${CUBESVIEWER_SERVER_HOST}:5000"
+
+#python /cubesviewer-server/cvapp/manage.py migrate && \
+#python /cubesviewer-server/cvapp/manage.py runserver 0.0.0.0:8000
+
+
